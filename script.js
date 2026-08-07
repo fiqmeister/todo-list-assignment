5577095d62046173e6d52eaf055f5691678bf961
fiqmeister <fiqmeister@gmail.com>
2026-08-07 10:07:00 +0000

    added script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   // all the code here
+  function main() {  
+    let todos = []; // store all the todos
+
+    // add three todos
+    addTodo(todos, "Walk the dog", "Medium", "Pending", "25-08-2026");
+    addTodo(todos, "Clean the room", "Low", "Completed", "16-08-2026");
+    addTodo(todos, "Pay the bill", "High", "In-Progress", "30-08-2026"); 
+  }
+
+  main();
   
 });
